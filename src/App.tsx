b4fb28c86ac26a95339cd748f2f0d19c1701e900
ronnie 3.0 <ronniemwanje94@gmail.com>
2025-03-02 07:40:12 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import TubeApp from './components/TubeApp'
+import TubeApp from './components/tube-app'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   
+      <TubeApp />
+    
+  )
+}
+
+export default App
